0e2f4a06c5092e57e8942adb44803d055e8f1585
Chakir Mrabet <cmrabet@innerspec.com>
2016-02-07 21:00:17 -0500

    Added icons to pages

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -13,4 +13,17 @@ $(function(){
 		e.preventDefault();
 		$('section.first').ScrollTo();
 	});
+
+	// Control of testimony pictures
+	$('div#testimony-pictures img').on('mouseover',function(){
+		if(!$(this).hasClass('selected')){
+			var id=$(this).attr('id');
+			$('div#testimony-pictures img').removeClass('selected');
+			$('div#testimony-content img').attr('src','/assets/img/ref-big-'+id+'.jpg');
+			$('div#testimony-content p').hide();
+			$('div#testimony-content p#text-'+id).fadeIn();
+			$(this).addClass('selected');
+		}
+		
+	});
 });
